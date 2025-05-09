b114b877330bfe35b48fdf21b94371a7b18dd067
alekulyn <alekulyn@users.noreply.github.com>
2025-05-10 05:35:26 +0900

    Implement all execv TODOS

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -47,7 +47,7 @@ fi
 
 echo "Removing the old writer utility and compiling as a native application"
 make clean
-make
+# make
 
 for i in $(seq 1 $NUMFILES); do
   ./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
